9d3e11b9d9c98beea391e189c0fe9a6471293d5d
Badar Tagar <genbadar@gmail.com>
2020-06-29 13:28:36 +0500

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+let msg = "Message 1:";
+console.log(msg);
+
+msg = "Message 2:";
+console.log(msg);
+
+msg = "Message 3:";
+console.log(msg);
+
+msg = "Message 4:";
+console.log(msg);
+
+msg = "Message 5:";
+console.log(msg); 
